937a6cd956dbacbbace8cc938f6fc132f933cbc7
Lukas <lovretic2002@gmail.com>
2024-11-19 21:00:48 +0100

    remove gui pupeter

--- scraping/imdbAndLetterboxdFunctions.js
+++ scraping/imdbAndLetterboxdFunctions.js
@@ -24,7 +24,7 @@ async function fillMoviesWithLetterboxdData(movies) {
 
   const browser = await puppeteer.launch({
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    headless: false,
+    // headless: false,
   });
 
   for (const movie of movies) {
