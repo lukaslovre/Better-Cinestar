f54147ba02d19be01dc8bf53647557b907aeb522
Lukas <lovretic2002@gmail.com>
2025-05-18 12:33:38 +0200

    delete files

--- client/build/_app/env.js
+++
@@ -1,1 +0,0 @@
-export const env={"PUBLIC_API_URL":"https://server.bettercinestar.lukaslovretic.from.hr"}
--- client/build/_app/immutable/entry/start.Cwvo9eJi.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/BarEQN8X.js";export{o as load_css,r as start};
